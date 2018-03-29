833e947154ac86966ce167236a02492b0168d535
Bodrudduja <sakib1674@gmail.com>
2018-03-29 16:17:39 +0600

    Modified Hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,11 +3,10 @@
 int main()
 {
 	
-	printf("Hello World... \n");
-	printf("Hello World... \n");
+	
 	printf("Hello World \n");
 	printf("GIT Workshop");	
 	printf("\n");
-	printf("Hello World..... \n");
+	
 	return 0;
 }
